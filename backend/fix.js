11c126f9f501750ef887bfdba8579e425a3d4305
Lancinet-dev <lancinetbalde21@gmail.com>
2026-05-21 08:37:17 +0000

    "derniere modification: 2026-05-21 08:36:11"

--- backend/fix.js
+++
@@ -1,4 +0,0 @@
-const pool = require('./config/database')
-pool.query("UPDATE users SET role = 'manager' WHERE id = 15")
-  .then(() => { console.log('✅ Thierno est maintenant Gérant'); process.exit(0) })
-  .catch(e => { console.error(e.message); process.exit(1) })
